fcb6346b4778f829115c5007de1b5d6d8b7e1d02
LYNX-kvm <kato.vanmulders07@gmail.com>
2025-10-21 20:27:51 +0200

    Add secrets list and update output format for gift assignment

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@ import random
 
 
 friends = ["Kato", "Kiany", "Chloe", "Stacey", "Luiz", "Karo"]
+secrets = ["dildo", "koekie", "honda", "naruto", "piepschuim", "anaal"]
 
 givers = friends[:]
 receivers = friends[:]
@@ -11,5 +12,6 @@ random.shuffle(receivers)
 while any(giver == receiver for giver, receiver in zip(givers, receivers)):
   random.shuffle(receivers)
 
-for giver, receiver in zip(givers, receivers):
-  print(f"{giver}, you are getting a present for {receiver}")
+for keyword, (giver, receiver) in zip(secrets, zip(givers, receivers)):
+  print(f'{keyword}: {{name:"{giver}", receiver:"{receiver}"}}')
+
